b5e9f40412d36746eaf17acdeda9d44673fc8e82
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-03-24 13:15:15 +0530

    Added Nav bar

---
+++ index.js
@@ -0,0 +1,6 @@
+function redirectToWhatsApp(){
+    window.location.href = 'https://wa.me/919502230420';
+}
+function redirectToInsta(){
+    window.location.href = "https://www.instagram.com/4_spaceinteriors/";
+}
